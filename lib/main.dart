7989a5b286580729660ac5651d1582ba611c0589
monis <monis5362413@gmail.com>
2019-12-09 20:07:49 +0800

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:the_basics/views/home/home_view.dart';
+// import 'package:the_basics/views/home/home_view.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +12,95 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         textTheme: Theme.of(context).textTheme.apply(fontFamily: 'Open Sans'),
       ),
-      home: HomeView(),
+      // home: HomeView(),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 70, vertical: 60),
+          alignment: Alignment.topCenter,
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 1200),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 80,
+                        width: 150,
+                        child: Image.asset('assets/logo.png'),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text('Episodes', style: TextStyle(fontSize: 18)),
+                          SizedBox(
+                            width: 60,
+                          ),
+                          Text('About', style: TextStyle(fontSize: 18)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 600,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'FLUTTER WEB.\nTHE BASICS',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w800,
+                                  height: 0.9,
+                                  fontSize: 80),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Text(
+                              'In this course we will go over the basics of using Flutter Web for website development. Topics will include Responsive Layout, Deploying, Font Changes, Hover Functionality, Modals and more.',
+                              style: TextStyle(
+                                fontSize: 21,
+                                height: 1.7,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 60, vertical: 15),
+                            child: Text(
+                              'Join Course',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w800,
+                                color: Colors.white,
+                              ),
+                            ),
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 31, 229, 146),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
