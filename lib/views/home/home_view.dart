c38b4a5b00a59724b5d02a3e9fa3b9cea50af341
monis <monis5362413@gmail.com>
2019-12-12 21:36:20 +0800

    final

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:the_basics/navigation_drawer/navigation_drawer.dart';
-import 'package:the_basics/views/home/home_content_Mobile.dart';
 import 'package:the_basics/views/home/home_content_desktop.dart';
+import 'package:the_basics/views/home/home_content_mobile.dart';
 import 'package:the_basics/widgets/centered_view/centered_view.dart';
 import 'package:the_basics/widgets/navigation_bar/navigation_bar.dart';
 
